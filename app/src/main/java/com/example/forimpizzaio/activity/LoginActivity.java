4b0ac977da3e2bb03d92fe91a2e99ae17239708c
rullkino <diferreira.rul@hotmail.com>
2019-08-27 23:00:41 -0300

    gffgf

--- app/src/main/java/com/example/forimpizzaio/activity/LoginActivity.java
+++ app/src/main/java/com/example/forimpizzaio/activity/LoginActivity.java
@@ -45,6 +45,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 startActivity(login);
 
 
+                
                 break;
         }
     }
